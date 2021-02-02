08931706ee7015c338c17d22c27f4ed1cbe3c4f1
Daniel Prieto Wilches <52753985+DanielWilches@users.noreply.github.com>
2021-02-02 11:05:00 -0500

    'cambios'

--- src/app/components/search-cards/search-cards.component.spec.ts
+++ src/app/components/search-cards/search-cards.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SearchCardsComponent } from './search-cards.component';
 
@@ -6,7 +6,7 @@ describe('SearchCardsComponent', () => {
   let component: SearchCardsComponent;
   let fixture: ComponentFixture<SearchCardsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SearchCardsComponent ]
     })
--- src/app/pages/pelicula/pelicula.component.spec.ts
+++ src/app/pages/pelicula/pelicula.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PeliculaComponent } from './pelicula.component';
 
@@ -6,7 +6,7 @@ describe('PeliculaComponent', () => {
   let component: PeliculaComponent;
   let fixture: ComponentFixture<PeliculaComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PeliculaComponent ]
     })
